cf7371bcfa38b28dadeb02eeaf54bd8bcaf6d908
Johann Ingi <joi.ingi@me.com>
2017-06-02 21:53:20 +0200

    added message to browser UI

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ io.on('connection', function (socket) {
     console.log('User connected via socket.io');
 
     socket.on('message', function (message) {
-        console.log('Message recived' +message.text);
+        console.log('Message recived: ' + message.text);
 
-        socket.broadcast.emit('message', message);
+        io.emit('message', message);
 
     });
 
